c8427e016c32bb0caac840a191ee850599f92762
zamtechie <zameer.inamdar2008@gmail.com>
2022-05-17 20:22:41 +0530

    2 files modified, 3 added

--- src/app/user/user-routing.module.ts
+++ src/app/user/user-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { UserComponent } from './list/user.component';
+import { AddUserComponent } from './add-user/add-user.component';
 
 const routes: Routes = [
   {
     path: '',
     component: UserComponent,
+  },
+  {
+    path: '/adduser',
+    component: AddUserComponent,
   },
 ];
 
